896574a0ce29fc10d093dbd83a61822526d7e6f1
Анна Посохова <Smolina.anna@bk.ru>
2022-12-13 21:24:47 +0300

    Task23

---
+++ Lesson3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int[] arr = CubeTable(number);
+
+int[] CubeTable(int n)
+{
+    int[] arr = new int[n];
+
+    for (int i = 1; i <= n; i++)
+    {
+        arr[i - 1] = i * i * i;
+    }
+
+    return arr;
+}
+
+System.Console.WriteLine(String.Join(",", arr));
+
